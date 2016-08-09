1a50fda2f39cf23aebb46bd8bf6807974431c7d4
m2cmyhy <mathew@nephilaweb.com.ph>
2016-08-09 18:20:11 +0800

    Layout design updated (Home page, Owner page, Passenger page, Login page)

--- resources/views/print/printsubsidiary.blade.php
+++ resources/views/print/printsubsidiary.blade.php
@@ -1,25 +1,63 @@
 <html>
     <head>
-        <style type='text/css' media='prints'>
-            .printer{
-                align-content:center;
+        <style type='text/css'>
+            .logo,.school{
+                width:60%
+            }
+            .logo{
+                width:40%;
+                text-align: right;
+            }
+            .info,.list{
+                width:80%;
+            }
+        </style>
+        <style type='text/css' media='print'>
+            .logo,.school{
+                width:70%
+            }
+            .logo{
+                width:30%;
+                text-align: right;
+            }
+            
+            .info,.list{
+                width:100%;
             }
         </style>
     </head>
     <body>
-        <div class='printer'>
-        <img src="{{asset('images/logo.png')}}">
-        <h3>Don Bosco Technical Institute, Makati</h3>
-        <p>Individual Account Summary </br>
-            Account Title : {{$request->accountname}}<br>
-        Date Covered : {{Date('M d,Y', strtotime($request->from))}} To {{Date('M d,Y',strtotime($request->to))}}
-        <table align="center"><tr><td>Tran Date</td><td>OR No</td><td>Name</td><td>Amount</td></tr>
-            @foreach($dblist as $dbl)
-            <tr><td>{{$dbl->transactiondate}}</td><td>{{$dbl->receiptno}}</td><td>
-                {{$dbl->lastname}}, {{$dbl->firstname}} {{$dbl->middlename}}
-                </td><td>{{$dbl->amount}}</td></tr>
-            @endforeach
-        </table>    
-        </div>
+        <table style="width: 100%;" cellpadding="10">
+            <thead>
+                <tr>
+                    <td class="logo"><img src="{{asset('images/logo.png')}}" style="display: inline-block"></td>
+                    <td class="school"><h3 style="display: inline-block;">Don Bosco Technical Institute, Makati</h3></td>
+                <tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="2">
+                        <div class="info" style="margin-left: auto; margin-right: auto;">
+                            <p>Individual Account Summary </br>
+                                Account Title : {{$request->accountname}}<br>
+                            Date Covered : {{Date('M d,Y', strtotime($request->from))}} To {{Date('M d,Y',strtotime($request->to))}}
+                            </p>
+                        </div>
+                        <table align="center" cellspacing="0" border="1" class="list">
+                            <thead>
+                                <tr><td><b>Tran Date</b></td><td><b>OR No</b></td><td><b>Name</b></td><td><b>Amount</b></td></tr>
+                            </thead>
+                            <tbody>
+                            @foreach($dblist as $dbl)
+                            <tr><td>{{$dbl->transactiondate}}</td><td>{{$dbl->receiptno}}</td><td>
+                                {{$dbl->lastname}}, {{$dbl->firstname}} {{$dbl->middlename}}
+                                </td><td>{{$dbl->amount}}</td></tr>
+                            @endforeach
+                            </tbody>
+                        </table>                            
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </body>
 </html>
