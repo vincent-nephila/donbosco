fc09525c7c847b3f1195bff7fd392aa23a2c43ea
m2cmyhy <mathew@nephilaweb.com.ph>
2016-12-21 14:18:34 +0800

    Update 12/21/16 	change from add to update status

--- resources/views/registrar/oldstudent.blade.php
+++ resources/views/registrar/oldstudent.blade.php
@@ -41,7 +41,7 @@
                          <strong style="color:red">Dropped</strong>
                         @else
                             <i>Registered</i> 
-                            <input type="hidden" name="action" value="add">
+                            <input type="hidden" name="action" value="update">
                         @endif
                         
                     @endif
